97c74196454b46f890e5eb1c4a5901a4c91cfd69
Pagnavathcoding <88227695+Pagnavathcoding@users.noreply.github.com>
2023-07-07 22:07:31 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,3 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://pagnavath:DwJkPDv69nVcN4mU@cluster0.dl6wikn.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://pagnavath:DwJkPDv69nVcN4mU@cluster0.dl6wikn.mongodb.net/?retryWrites=true&w=majority"{
+  useNewUrlParser: "true",
+  useUnifiedTopology: "true"
+});
