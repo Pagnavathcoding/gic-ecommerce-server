7c5fcbe3bf0b9d6ba8cafa904344874e92c8c141
Pagnavathcoding <88227695+Pagnavathcoding@users.noreply.github.com>
2023-07-07 22:01:52 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,3 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DATABASE_CONNECTION).then(() => {
-    console.log("DB Connected!");
-}).catch(err => console.log(err));
+mongoose.connect(process.env.DATABASE_CONNECTION);
